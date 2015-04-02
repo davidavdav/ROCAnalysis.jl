379cf2ae751a0a4926557ba1c4fa123c3ecb976c
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2015-04-02 13:39:54 +0200

    DataFrame -> AbstractDataFrame

--- src/dataframes.jl
+++ src/dataframes.jl
@@ -6,7 +6,7 @@
 ## We also implement most TNT arguments here.
 
 ## Accept a DataFrame with :target and :score columns to most functions
-function TNT(x::DataFrame; score=:score, target=:target)
+function TNT(x::AbstractDataFrame; score=:score, target=:target)
     t = array(x[target])
     s = array(x[score])
     TNT(s[t], s[!t])
@@ -14,22 +14,22 @@ end
 
 for f in (:eer, :eerch, :auc, :cllr, :mincllr)
     @eval ($f)(tnt::TNT) = ($f)(tnt.tar, tnt.non)
-    @eval ($f)(x::DataFrame; score=:score, target=:target) = ($f)(TNT(x, score=score, target=target))
+    @eval ($f)(x::AbstractDataFrame; score=:score, target=:target) = ($f)(TNT(x, score=score, target=target))
 end
 
-roc(x::DataFrame; score=:score, target=:target, laplace=false, collapse=true) = roc(TNT(x, score=score, target=target), laplace=laplace, collapse=collapse)
+roc(x::AbstractDataFrame; score=:score, target=:target, laplace=false, collapse=true) = roc(TNT(x, score=score, target=target), laplace=laplace, collapse=collapse)
 roc(tnt::TNT; laplace=false, collapse=true) = roc(tnt.tar, tnt.non, laplace=laplace, collapse=collapse)
 
 for f in (:ber, :minber)
     @eval ($f)(tnt::TNT, plo) = ($f)(tnt.tar, tnt.non, plo)
-    @eval ($f)(x::DataFrame, plo; score=:score, target=:target) = ($f)(TNT(x, score=score, target=target), plo)
+    @eval ($f)(x::AbstractDataFrame, plo; score=:score, target=:target) = ($f)(TNT(x, score=score, target=target), plo)
 end
 
 dcf(tnt::TNT; d::DCF=getdcf(), thres=-plo(d), norm=false) = dcf(tnt.tar, tnt.non, d=d, thres=thres, norm=norm)
-dcf(x::DataFrame; score=:score, target=:target, d::DCF=getdcf(), thres=-plo(d), norm=false) = dcf(TNT(x, score=score, target=target), d=d, thres=thres, norm=norm)
+dcf(x::AbstractDataFrame; score=:score, target=:target, d::DCF=getdcf(), thres=-plo(d), norm=false) = dcf(TNT(x, score=score, target=target), d=d, thres=thres, norm=norm)
 
 mindcf(tnt::TNT; d::DCF=getdcf(), norm=false) = mindcf(tnt.tar, tnt.non, d=d, norm=norm)
-mindcf(x::DataFrame; score=:score, target=:target, d::DCF=getdcf(), norm=false) = mindcf(TNT(x, score=score, target=target), d=d, norm=norm)
+mindcf(x::AbstractDataFrame; score=:score, target=:target, d::DCF=getdcf(), norm=false) = mindcf(TNT(x, score=score, target=target), d=d, norm=norm)
 
 
 DataFrames.DataFrame(r::Roc) = DataFrame(pfa=r.pfa, pmiss=r.pmiss, thres=[DataArray(r.θ), NA], chull=r.ch, llr=[DataArray(r.llr), NA])
